cc043412d0af7b3ec0189148ae271dc717762fe4
Pedro-V8 <pedrophdv@outlook.com>
2022-03-26 21:34:50 -0300

    Adicionando bor telegram

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -7,21 +7,25 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import AllSlotsReset
+import requests
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world2"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        #nome = tracker.get_slot("name")
+        #request = requests.get('https://dadosabertos.camara.leg.br/api/v2/deputados?nome=%s&ordem=ASC&ordenarPor=nome'%nome).json()
+        #print(request["dados"][0]["nome"])
+        #print(nome)
+        dispatcher.utter_message(text="Hello World!123")
+
+        return []
